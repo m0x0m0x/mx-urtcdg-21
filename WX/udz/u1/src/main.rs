7d2cbc24d497527d3c8df485e46d693e2d5f6120
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-08 11:34:09 +0000

    🫦

--- WX/udz/u1/src/main.rs
+++ WX/udz/u1/src/main.rs
@@ -5,7 +5,8 @@ Main entry point for the Rust application.
 mod s1;
 mod utils;
 
-use s1::w1::s1_main
-use utils::{header, pswg};
+use s1::w1::s1_main;
 
-fn main() {}
+fn main() {
+    s1_main();
+}
