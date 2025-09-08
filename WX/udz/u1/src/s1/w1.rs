fef2aeb15b1c6887189b90b578608e616528ef21
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-08 11:30:46 +0000

    🫦

--- WX/udz/u1/src/s1/w1.rs
+++ WX/udz/u1/src/s1/w1.rs
@@ -1,7 +1,16 @@
-// Section work will be here , wills start with w1.rs and so on also they called from mod.rs
+/*
+Section work will be here , wills start with w1.rs and so on also they called from mod.rs
+*/
 
 // --- Imports ---
+use crate::utils::{header, pswg};
 
 // --- Main Function ---
 
+pub fn s1_main() {
+    pswg("Section1 - Work1")
+}
+
 // --- Sub Functions---
+
+fn greet() {}
