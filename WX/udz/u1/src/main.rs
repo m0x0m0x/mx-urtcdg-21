562f2a04419ebafadfe621194a8a7516eeb264dc
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-08 14:14:40 +0000

    🫦

---
+++ WX/udz/u1/src/main.rs
@@ -0,0 +1,12 @@
+/*
+Main entry point for the Rust application.
+*/
+
+mod s1;
+mod utils;
+
+use s1::s1::s1_main as s1m;
+
+fn main() {
+    s1m()
+}
---
+++ WX/udz/u1/src/s1/s1.rs
@@ -0,0 +1,22 @@
+/*
+Section work will be here , wills start with w1.rs and so on also they called from mod.rs
+*/
+
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+// --- Imports ---
+use crate::utils::{header, pswg};
+
+// --- Main Function ---
+pub fn s1_main() {
+    greet();
+}
+
+// --- Sub Functions---
+
+// Greet function
+
+fn greet() {
+    pswg("Section 1 Work".to_string())
+}
