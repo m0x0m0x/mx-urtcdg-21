3172e0082e7f301e4fc621858c4ec8eb3b613182
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-20 13:53:32 +0000

    🫦

---
+++ WX/udz/u1/src/s5/l1.rs
@@ -0,0 +1,18 @@
+/*
+l1.ra
+- This rust file is for writing learning patterns
+*/
+
+// -- Directives ---
+
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+// --- Imports ---
+use crate::utils::{header, pswg};
+
+use yansi::Paint;
+
+// --- Main Function ---
+
+// --- Sub Functions---
