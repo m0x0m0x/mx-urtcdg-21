d804b2aac556cf82ab6922ef7e5108f330a9cff6
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-27 16:07:54 +0000

    🫦

--- WX/udz/u1/src/s5/l1.rs
+++ WX/udz/u1/src/s5/l1.rs
@@ -163,4 +163,12 @@ fn pr_ab() {
 
     //-- Printing individual variants from the enum ---
     // println!("{:#?}", catalog.items.get(10));
+    match catalog.items.get(0) {
+        Option::Some(value) => {
+            println!("Item: {:#?}", value);
+        }
+        Option::None => {
+            println!("None");
+        }
+    }
 }
