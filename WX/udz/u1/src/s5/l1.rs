1e86b1e5ec1eeb93659773e30ff5dc24f2086ede
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-09-27 15:32:17 +0000

    🫦

--- WX/udz/u1/src/s5/l1.rs
+++ WX/udz/u1/src/s5/l1.rs
@@ -40,6 +40,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String, year: i32 },
+    Podcast(u32),
+    Placeholder,
 }
 
 // Implementation of every variants - On Implementation Block
