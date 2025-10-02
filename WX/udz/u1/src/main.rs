ad362e4c3f70eb98b414c1ea78bbc82beb7b19bb
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-10-02 12:40:59 +0000

    🫦

--- WX/udz/u1/src/main.rs
+++ WX/udz/u1/src/main.rs
@@ -2,13 +2,14 @@
 Main entry point for the Rust application.
 */
 
-mod s5;
+mod s7;
 mod utils;
 
 // use s2::s2::s2_main as s2m;
 // use s5::w1::s5_w1_main as s5m;
-use s5::l1::s5_l1_main as s5lm;
+// use s5::l1::s5_l1_main as s5lm;
+use s7::w1::s7_w1_main as s7m;
 
 fn main() {
-    s5lm();
+    s7m();
 }
--- WX/udz/u1/src/s7/w1.rs
+++ WX/udz/u1/src/s7/w1.rs
@@ -9,3 +9,15 @@ Section 7 - Section 7 work here
 use crate::utils::{header, pswg};
 use rand::{rng, seq::SliceRandom};
 use yansi::Paint;
+
+// --- Main Function ---
+
+pub fn s7_w1_main() {
+    greet();
+}
+
+// --- Sub Functions ---
+
+fn greet() {
+    pswg("Sec7 - Section 7 work here".to_string())
+}
