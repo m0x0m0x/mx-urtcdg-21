e1186d0c951ca1a5139ff7750d73f9a1a49f0027
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-10-03 02:31:10 +0000

    🫦

--- WX/udz/u1/src/s7/l1.rs
+++ WX/udz/u1/src/s7/l1.rs
@@ -41,5 +41,5 @@ fn divide(a: f64, b: f64) -> Result<f64, Error> {
 
 fn div1() {
     let a1 = divide(10.0, 2.0);
-    println!("{} = {}", "10/2 is ", a1.unwrap());
+    println!("{} = {:#?}", "10/2 is ", a1.blue());
 }
