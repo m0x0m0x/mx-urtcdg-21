7e8d3dd2e8c53be0ac48668077a6ef85280bacc8
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-10-03 02:34:34 +0000

    🫦

--- WX/udz/u1/src/s7/l1.rs
+++ WX/udz/u1/src/s7/l1.rs
@@ -40,7 +40,7 @@ fn divide(a: f64, b: f64) -> Result<f64, Error> {
 // Implement above function
 
 fn div1() {
-    header("Implementing the divide function");
+    header("Div1 - Function");
     let a1 = divide(10.0, 2.0);
     println!("{} = {}", "10/2 is ", a1.unwrap().blue());
 }
