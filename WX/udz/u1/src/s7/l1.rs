9545fa0298b8d17c224e596a75be104d46c78a06
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-10-03 02:24:23 +0000

    🫦

--- WX/udz/u1/src/s7/l1.rs
+++ WX/udz/u1/src/s7/l1.rs
@@ -29,7 +29,7 @@ fn greet() {
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {
     if b == 0.0 {
-        return Err(Error::DivideByZero);
+        Err(Error::other("Fucker no division by zero"))
     }
     Ok(a / b)
 }
