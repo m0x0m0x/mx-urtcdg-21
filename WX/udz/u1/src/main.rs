f78270ef8c2c893389df8ab9d4919dd12886e320
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2025-10-06 11:05:43 +0000

    🫦

--- WX/udz/u1/src/main.rs
+++ WX/udz/u1/src/main.rs
@@ -8,9 +8,9 @@ mod utils;
 // use s2::s2::s2_main as s2m;
 // use s5::w1::s5_w1_main as s5m;
 // use s5::l1::s5_l1_main as s5lm;
-// use s7::w1::s7_w1_main as s7m;
-use s7::l1::s7_l1_main as s7m;
+use s7::w1::s7_w1_main as s7mw1;
+// use s7::l1::s7_l1_main as s7ml1;
 
 fn main() {
-    s7m();
+    s7mw1();
 }
